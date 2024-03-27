17563dd45b9818bc0b9735bc29a7498060daf612
Devendrasingh Solanki <dsoulanki49@gmail.com>
2024-03-27 21:35:22 +0530

    updated

--- US_visa_project/utils/main_utils.py
+++ US_visa_project/utils/main_utils.py
@@ -6,8 +6,8 @@ import dill
 import yaml
 from pandas import DataFrame
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
+from US_visa_project.exception import USvisaException
+from US_visa_project.logger import logging
 
 
 def read_yaml_file(file_path: str) -> dict:
